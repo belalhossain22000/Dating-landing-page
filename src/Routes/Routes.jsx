9d0f51677376e24108400e03ff8394ffec64fafb
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-07-05 21:08:09 +0600

    added error Page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import {
 } from "react-router-dom";
 import Main from "../Layout/Main/Main";
 import Home from "../components/Pages/Home/Home/Home";
+import ErrorPage from "../components/Pages/Shared/ErrorPage/ErrorPage";
 
 
 
@@ -11,6 +12,7 @@ export const router = createBrowserRouter([
     {
         path: "/",
         element: <Main/>,
+        errorElement:<ErrorPage/>,
         children:[
             {
                 path:"/",
